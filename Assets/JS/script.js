71b721e05bfe25f0c939a4c7b8674e6a591c5323
Donald Claxton <blackjupitermusic1@gmail.com>
2020-11-04 14:46:01 -0700

    debugging

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,44 +1,43 @@
 const api = {
     key: "afaf9f8d48cff6cafd32e23220bcfdbf",
     base: "https://api.openweathermap.org/data/2.5/"
-}
-
-const searchbox = document.querySelector('.search-box'); 
-searchbox.addEventListener('keypress', setQuery);
-
-function setQuery(evt) {
+  }
+  
+  const searchbox = document.querySelector('.search-box');
+  searchbox.addEventListener('keypress', setQuery);
+  
+  function setQuery(evt) {
     if (evt.keyCode == 13) {
-        getResults(searchbox.value);
+      getResults(searchbox.value);
     }
-}
-
-function getResults (query) {
+  }
+  
+  function getResults (query) {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => {
-        return weather.json(); 
-    }).then(displayResults); 
-}
-
-function displayResults (weather) {
+      .then(weather => {
+        return weather.json();
+      }).then(displayResults);
+  }
+  
+  function displayResults (weather) {
     let city = document.querySelector('.location .city');
-    city.innerText =  `${weather.name}, ${weather.sys.country}`;
-
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+  
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
-    
+  
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-
-    let weather_el = document.querySelector('.current .temp')
-    weather_el.innertext = weather.weather[0].main;
-
-    let hilow = document.querySelector('.hi-low')
-    hilow.innerText= `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-
-}
-
-function dateBuilder (d) {
+  
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+  
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  }
+  
+  function dateBuilder (d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   
